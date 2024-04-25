4352cbe2fe98396ca9422c533b7e0f14dc0b2a25
Winged002 <andrejdimov2@gmail.com>
2024-04-25 10:46:38 +0300

    Added GTK Toolkit

--- app.make
+++ app.make
@@ -1,6 +1,6 @@
 CC = gcc
-CFLAGS = -Wall -Wextra -std=c11
-LIBS = -lm
+CFLAGS = -Wall -Wextra -std=c11 `pkg-config --cflags gtk+-3.0`
+LIBS = -lm `pkg-config --libs gtk+-3.0`
 
 main: main.o bcdopps.o
 	$(CC) $(CFLAGS) -o main main.o bcdopps.o $(LIBS)
--- main.c
+++ main.c
@@ -1,17 +1,27 @@
+#include <gtk/gtk.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include "bcdopps.h"
 
-int main() {
-    int a, b;
-    char sign;
-    while (true) {
-        printf("Enter first number: \n");
-        scanf("%d", &a);
-        printf("Enter operation sign \n");
-        scanf(" %c", &sign);
-        printf("Enter second number \n");
-        scanf("%d", &b);
+static void button_clicked(GtkWidget *widget, gpointer data) {
+    // Handle button click event
+    char *label = gtk_button_get_label(GTK_BUTTON(widget));
+    g_print("Button clicked: %s\n", label);
+
+    // If the button is a number or operator button
+    if (strcmp(label, "=") != 0) {
+        // Concatenate the clicked button label to the entry
+        const gchar *text = gtk_entry_get_text(GTK_ENTRY(data));
+        gchar *new_text = g_strconcat(text, label, NULL);
+        gtk_entry_set_text(GTK_ENTRY(data), new_text);
+        g_free(new_text);
+    } else {
+        // If "=" button is clicked, evaluate the expression
+        const gchar *expression = gtk_entry_get_text(GTK_ENTRY(data));
+        // Convert the expression from string to integer operation
+        int a, b;
+        char sign;
+        sscanf(expression, "%d %c %d", &a, &sign, &b);
         switch (sign) {
             case '+':
                 add(a, b);
@@ -33,5 +43,35 @@ int main() {
                 break;
         }
     }
+}
+
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+
+    GtkWidget *window;
+    GtkWidget *grid;
+    GtkWidget *entry;
+    GtkWidget *button;
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    grid = gtk_grid_new();
+    gtk_container_add(GTK_CONTAINER(window), grid);
+
+    entry = gtk_entry_new();
+    gtk_grid_attach(GTK_GRID(grid), entry, 0, 0, 4, 1);
+
+    char *button_labels[16] = {"7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", "^", "=", "+"};
+    for (int i = 0; i < 16; i++) {
+        button = gtk_button_new_with_label(button_labels[i]);
+        g_signal_connect(button, "clicked", G_CALLBACK(button_clicked), entry);
+        gtk_grid_attach(GTK_GRID(grid), button, i % 4, i / 4 + 1, 1, 1);
+    }
+
+    gtk_widget_show_all(window);
+
+    gtk_main();
+
     return 0;
 }
